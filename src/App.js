ac80892e032d4f2dbdfbe871a0021af5a63e8c8f
Wilmer Martinez <wmartinez.mail@gmail.com>
2022-11-30 01:48:47 -0800

    add about page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import './App.css';
+import React from 'react';
+import About from './components/About';
+import Nav from './components/Nav';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div>
+			<Nav></Nav>
+			<main>
+				<About></About>
+			</main>
 		</div>
 	);
 }
-
 export default App;
